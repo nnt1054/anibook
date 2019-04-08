7953bf6fdf5bbae0151cc654e7ebc7f83e652017
Neil Toledo <ntoledo@berkeley.edu>
2019-04-07 23:41:17 -0700

    created sidenav react components based on mockup

--- src/App.js
+++ src/App.js
@@ -30,24 +30,107 @@ class App extends Component {
             React.createElement('div', {
                 className: 'app'
             }, [
-                React.createElement(SideNav, {
-                    className: 'side-nav',
-                }, null),
-                // React.createElement('div', {
-                //     className: 'main-content'
-                // }, [])
+                React.createElement(SideNav, null, null),
             ])
         )
     }
 }
 
 class SideNav extends Component {
+
+    onToggleGenreBtn() {
+      alert('HEY LMAO');
+    }
+
     render() {
+        const genres = ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy']
         return (
-            React.createElement('h5', null, 'lmao hey'),
-            React.createElement('h5', null, 'hey haha')
+            React.createElement('div', {
+              className: 'side-nav',
+            }, [
+              // logo-container-container
+              React.createElement('div', {
+                className: 'nav-logo-container'
+              }, [
+                React.createElement('img', {
+                  className: 'nav-logo-image',
+                  src: logo,
+                })]),
+              // search-textbox (component)
+              React.createElement(SearchTextBox),
+              // your-list-btn
+              React.createElement('button', {
+                id: 'your-list-btn',
+                className: 'nav-btn',
+              }, 'Your List'),
+              // season-btn
+              React.createElement('button', {
+                id: 'season-btn',
+                className: 'nav-btn',
+              }, 'By Season'),
+              // popular-btn
+              React.createElement('button', {
+                id: 'popular-btn',
+                className: 'nav-btn',
+              }, 'Popular'),
+              // genre-title-container
+              // <div id='genre-title-container' class="row"><span> Genres </span><button id='toggleGenreBtn' onclick='toggleGenreList()'>v</button></div>
+              React.createElement('div', {
+                id: 'genre-title-container',
+                className: 'row',
+              }, [
+                React.createElement('span', null, 'Genres'),
+                React.createElement('button', {
+                  id: 'toggleGenreBtn',
+                  onClick: this.onToggleGenreBtn,
+                }, 'v')
+                ]
+              ),
+              // genre-container (component)
+              React.createElement(GenreContainer, {
+                genres: genres,
+              }),
+            ])
         )
     }
 }
 
+class SearchTextBox extends Component {
+  render() {
+    return (
+      // <input id="search-textbox" type="text" name="location" placeholder="Search..." value="">
+      React.createElement('input', {
+        id: 'search-textbox', 
+        type: 'text',
+        name: 'location',
+        placeholder: 'Search...',
+      }, null)
+    )
+  }
+}
+
+class GenreContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      genres: props.genres,
+    }
+  }
+  
+  render() {
+    const genreButtons = this.state.genres.map(genre =>
+        React.createElement('span', null, genre)
+      )
+    // this.items = this.state.cart.map((item, key) =>
+    //   <li key={item.id}>{item.name}</li>
+    // );
+
+    return React.createElement('div', {
+      // id: 'genre-container',
+      className: 'column'
+    }, genreButtons)
+  }
+}
+
 export default App;
+
